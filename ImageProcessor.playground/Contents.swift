5e125773126fb3bb0091a7643911529e7dea4131
José <jose.epifanio90@gmail.com>
2016-05-01 12:38:29 -0500

    Added a class and created some filters.

--- ImageProcessor.playground/Contents.swift
+++ ImageProcessor.playground/Contents.swift
@@ -8,23 +8,53 @@ let image = UIImage(named: "sample")
 
 var rgbaImage = RGBAImage(image: image!)
 
-
-for y in 0..<rgbaImage!.height
+class ImagesFilter
 {
-    for x in 0..<rgbaImage!.width
+
+    func NegativeFilter()
     {
-        var index = y*rgbaImage!.width+x
-        var pixel = rgbaImage?.pixels[index]
-        var red = 255 - (pixel?.red)!
-        var green = 255-(pixel?.green)!
-        var blue = 255-(pixel?.blue)!
-        pixel?.red = UInt8(red)
-        pixel?.green = UInt8(green)
-        pixel?.blue = UInt8(blue)
-        rgbaImage?.pixels[index] = pixel!
+        for y in 0..<rgbaImage!.height
+        {
+            for x in 0..<rgbaImage!.width
+            {
+                var index = y*rgbaImage!.width+x
+                var pixel = rgbaImage?.pixels[index]
+                var red = 255 - (pixel?.red)!
+                var green = 255-(pixel?.green)!
+                var blue = 255-(pixel?.blue)!
+                pixel?.red = UInt8(red)
+                pixel?.green = UInt8(green)
+                pixel?.blue = UInt8(blue)
+                rgbaImage?.pixels[index] = pixel!
+            }
+        }
+
+        let negativeImage = rgbaImage?.toUIImage()
+        print(negativeImage)
+    }
+
+    func GrayScaleFilter()
+    {
+         for y in 0..<rgbaImage!.height
+        {
+            for x in 0..<rgbaImage!.width
+            {
+                var index = y*rgbaImage!.width+x
+                var pixel = rgbaImage?.pixels[index]
+                var gray = int(0.21 * (pixel?.red)! + 0.72 * (pixel?.green)! + 0.07 * (pixel?.blue)!)
+                pixel?.red = UInt8(gray)
+                pixel?.green = UInt8(gray)
+                pixel?.blue = UInt8(gray)
+                rgbaImage?.pixels[index] = pixel!
+            }
+        }
+
+        let grayScaleImage = rgbaImage?.toUIImage()
+        print(grayScaleImage)
+    }
+
+
+    func ContrastFilter()
+    {
     }
 }
-
-let grayScale = rgbaImage?.toUIImage()
-
-print(grayScale)
